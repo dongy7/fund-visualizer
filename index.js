130fd8d01ce327792cca59e082c31d33cdf80db1
dongy7 <dongy7@gmail.com>
2016-05-04 16:43:18 -0400

    reorganized logic into src directory

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import configureStore from './store/configureStore'
+import configureStore from './src/store/configureStore'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 import { Router } from 'react-router'
-import getRoutes from './components/root'
+import getRoutes from './src/components/root'
 import {syncReduxAndRouter} from 'redux-simple-router'
 
 
