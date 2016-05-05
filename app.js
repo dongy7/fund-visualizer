6a96c630898b6a583ab0cc18ee252cf0f83ef92b
dongy7 <dongy7@gmail.com>
2016-05-04 20:19:08 -0400

    cleaned up express

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var express = require('express')
-var app = new (express)()
-var port = (process.env.PORT | 3000) 
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 3000));
 
 app.use('/static', express.static('static'));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/index.html')
-})
+});
 
-app.listen(port, function(error) {
+app.listen(app.get('port'), function(error) {
   if (error) {
     console.error(error)
   } else {
+	port = app.get('port')
     console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
   }
-})
+});
